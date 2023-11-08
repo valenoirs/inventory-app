59b0c6acfa9c9984db0c92b48a2b56c3b85f8083
valenoirs <luckyvw.general@gmail.com>
2023-11-08 09:16:58 +0700

    update build preferences

--- copy.js
+++ copy.js
@@ -1,8 +1,11 @@
 const fs = require('fs-extra')
 
-fs.copySync('./src/views', './_build/views')
-fs.copySync('./src/public', './_build/public')
+fs.copySync('./.build', './build')
+fs.copySync('./src/views', './build/views')
+fs.copySync('./src/public/image', './build/public/image')
 fs.copySync(
   './src/utils/apriori-obfuscated.js',
-  './_build/utils/apriori-obfuscated.js'
+  './build/utils/apriori-obfuscated.js'
 )
+
+fs.mkdirSync('./build/public/upload')
